0692cd95752e717aaf5a22ecc944d3035f1697e7
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2024-08-01 18:06:08 +0700

    added the basic font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {AppProps} from 'next/app';
 import {Provider} from 'react-redux';
+import '../styles/globals.scss'
 import store from '../store';
 import {setTheme} from '../store/slices/themeSlice';
 import {fetchExchangeRates} from '../store/slices/currencySlice';
@@ -10,10 +11,8 @@ const RusGumroad = ({Component, pageProps}: AppProps) => {
 
     useEffect(() => {
         const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-        //@ts-ignore
         store.dispatch(setTheme(theme));
-        //@ts-ignore
-        store.dispatch(fetchExchangeRates());
+        store.dispatch(fetchExchangeRates() as any);
     }, []);
 
     return (
