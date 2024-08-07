8274dab98be4dc7527d18c1d6aa2340aa7d4194a
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2024-08-07 21:57:30 +0700

    working with coins

--- components/MainSection/MainSection.tsx
+++ components/MainSection/MainSection.tsx
@@ -4,23 +4,26 @@ import styles from './styles.module.scss';
 
 const MainSection = () => {
     return (
-        <div className={'container'}>
+        <div className={`${styles.container}`}>
+            <img src={'/mainScreen/coin1.webp'} alt={'RusGumroad'}
+                 className={`${styles.coin} ${styles.coin1}`}
+                 width={81} height={96}/>
+            <img src={'/mainScreen/coin2.webp'} alt={'RusGumroad'}
+                 className={`${styles.coin} ${styles.coin2}`}
+                 width={75} height={76}/>
+            <img src={'/mainScreen/coin3.webp'} alt={'RusGumroad'}
+                 className={`${styles.coin} ${styles.coin3}`}
+                 width={124} height={131}/>
+            <img src={'/mainScreen/coin4.webp'} alt={'RusGumroad'}
+                 className={`${styles.coin} ${styles.coin4}`}
+                 width={147} height={148}/>
+            <img src={'/mainScreen/coin5.webp'} alt={'RusGumroad'}
+                 className={`${styles.coin} ${styles.coin5}`}
+                 width={96} height={82}/>
             <section className={`${styles.main}`}>
-                <img src={'/mainScreen/coin1.webp'} alt={'RusGumroad'}
-                     className={`${styles.coin} ${styles.coin1}`}
-                     width={81} height={96}/>
-                <img src={'/mainScreen/coin2.webp'} alt={'RusGumroad'}
-                     className={`${styles.coin} ${styles.coin2}`}
-                     width={75} height={76}/>
-                <img src={'/mainScreen/coin3.webp'} alt={'RusGumroad'}
-                     className={`${styles.coin} ${styles.coin3}`}
-                     width={124} height={131}/>
-                <img src={'/mainScreen/coin4.webp'} alt={'RusGumroad'}
-                     className={`${styles.coin} ${styles.coin4}`}
-                     width={147} height={148}/>
-                <img src={'/mainScreen/coin5.webp'} alt={'RusGumroad'}
-                     className={`${styles.coin} ${styles.coin5}`}
-                     width={96} height={82}/>
+                <div className={`${styles.mainText}`}>
+                    Простой и удобный способ покупки продуктов <h1 className={`${styles.mainTitle}`}>Gumroad</h1>  в&nbsp;России
+                </div>
                 <GumroadButton href={'https://gumroad.com/'} revert={true} className={`${styles.button}`}>
                     Перейти на сайт GUMROAD
                 </GumroadButton>
