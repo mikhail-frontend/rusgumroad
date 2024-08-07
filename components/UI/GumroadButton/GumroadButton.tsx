a051ebec67475e67e26e4da894b773c0b6def86e
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2024-08-07 16:25:12 +0700

    start dev the main section

--- components/UI/GumroadButton/GumroadButton.tsx
+++ components/UI/GumroadButton/GumroadButton.tsx
@@ -3,9 +3,9 @@ import Ripple from './Ripple';
 import styles from './styles.module.scss';
 import Link from "next/link";
 
-const GumroadButton = ({ href = '#', target = '_blank', className = '', children }) => {
+const GumroadButton = ({ href = '#', target = '_blank', className = '',  revert = false, children }) => {
     return (
-        <Ripple className={`${styles.button} ${className}`}>
+        <Ripple className={`${styles.button} ${className} ${revert ? styles.revert : ''}`}>
             <Link href={href} role={'button'} target={target} className={styles.link}>
                 {children}
             </Link>
